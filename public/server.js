30670f0f88f3cb662828cc13c2de731049c41faa
Murtsi <antti.murtsi@gmail.com>
2024-07-08 22:21:58 +0300

    Initial commit

---
+++ public/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let messages = []; // Array to store chat messages
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // Send stored messages to the client
+  socket.emit('chat history', messages);
+
+  socket.on('chat message', (data) => {
+    messages.push(data); // Store the message
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
